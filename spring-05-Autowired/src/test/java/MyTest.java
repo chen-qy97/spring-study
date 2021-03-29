dd760bf732404c58d40ebb2291e29af62c5c4501
chenmin <chenmin@bonc.com.cn>
2021-03-29 14:11:20 +0800

    新增：autowired

--- spring-05-Autowired/src/test/java/MyTest.java
+++ spring-05-Autowired/src/test/java/MyTest.java
@@ -2,6 +2,14 @@ import com.kuang.pojo.People;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * bean的自动装配
+ * 1.自动装配是Spring满足bean依赖一种方式
+ * 2.Spring会在上下文中自动寻找，并自动给bean装配属性
+ *
+ *
+ *
+ */
 public class MyTest {
 
     public static void main(String[] args) {
